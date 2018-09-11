af6bbc84a3ff40682cec69e959f89c4b796a82a7
张佃磊 <532219308@qq.com>
2018-09-11 10:25:18 +0800

    数据预处理

--- src/day1.py
+++ src/day1.py
@@ -8,4 +8,43 @@
 
 """
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
+# 使用pandas导入管理数据集
+dataset = pd.read_csv('../datasets/Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
+# 使用sklearn处理缺失数据
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# 解析分类数据
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+
+# 创建虚拟变量
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+# 拆分训练数据集和测试数据集
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+print("归一化前：")
+print(X_test, "\n----\n", X_train)
+print(Y_test, "\n----\n", Y_train)
+
+# 归一化处理
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+
+print("\n归一化后：")
+print(X_test, "\n----\n", X_train)
+print(Y_test, "\n----\n", Y_train)
